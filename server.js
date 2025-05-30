8c69e8643bdf2bfda2b8d2786d3e7ab433622ae0
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2025-05-30 18:14:24 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,42 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const { spawn } = require("child_process");
 
-const express = require('express');
-const { exec } = require('child_process');
-const bodyParser = require('body-parser');
-const db = require('./db');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
-db.initDB();
-
-app.post('/run', async (req, res) => {
-  const cmd = req.body.command;
-  exec(cmd, async (error, stdout, stderr) => {
-    const output = stdout + (stderr || '');
-    await db.query('INSERT INTO commands(command_text, output_text) VALUES ($1, $2)', [cmd, output]);
-    res.send({ output });
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  socket.on("command", (cmd) => {
+    const shell = spawn(cmd, { shell: true });
+    shell.stdout.on("data", (data) => {
+      socket.emit("output", data.toString());
+    });
+    shell.stderr.on("data", (data) => {
+      socket.emit("output", data.toString());
+    });
+    shell.on("close", (code) => {
+      socket.emit("output", `\n[Process exited with code ${code}]\n`);
+    });
   });
 });
 
-app.get('/history', async (req, res) => {
-  const result = await db.query('SELECT command_text, output_text FROM commands ORDER BY id DESC LIMIT 50');
-  res.send(result.rows);
-});
-
-app.post('/install', async (req, res) => {
-  const pkg = req.body.package;
-  await db.query('INSERT INTO packages(name) VALUES ($1)', [pkg]);
-  res.send({ status: 'installed', package: pkg });
-});
-
-app.post('/login', async (req, res) => {
-  const { service, credential } = req.body;
-  await db.query('INSERT INTO logins(service, credential) VALUES ($1, $2)', [service, credential]);
-  res.send({ status: 'logged in', service });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port 3000");
 });
